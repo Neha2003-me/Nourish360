6c26e0f339b38280147311d9bb2a83bc4e8db376
Neha Singh <143337951+Neha2003-me@users.noreply.github.com>
2024-02-09 00:59:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+(function() {
+    if (document.querySelector(".top-bar__nav-toggle")) {
+      var navToggle = document.querySelector(".top-bar__nav-toggle");
+  
+      function watchNavClose(e) {
+        var topNav = document.querySelector(".top-bar");
+        if (!e.path.includes(topNav)) {
+          openCloseNav();
+          document.documentElement.removeEventListener("click", watchNavClose);
+        }
+      }
+  
+      function openCloseNav() {
+        var navToggle = document.querySelector(".top-bar__nav-toggle");
+  
+        if (!navToggle.classList.contains("closed")) {
+          navToggle.classList.add("closed");
+          document.querySelector(".top-bar__nav").classList.remove("collapsed");
+          document.querySelector("html").addEventListener("click", watchNavClose);
+        } else {
+          navToggle.classList.remove("closed");
+          document.querySelector(".top-bar__nav").classList.add("collapsed");
+          document.documentElement.removeEventListener("click", watchNavClose);
+        }
+      }
+  
+      navToggle.addEventListener("click", openCloseNav);
+    }
+  })();
 let slideIndex = 0;
 showSlides();
 
